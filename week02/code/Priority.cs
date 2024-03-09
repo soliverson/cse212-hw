9c22ae79de94cf3bce7f084fe0d22926a57e1254
soliverson <stacy.oliverson@gmail.com>
2024-03-09 12:36:16 -0700

    Finished Week 2

--- week02/code/Priority.cs
+++ week02/code/Priority.cs
@@ -1,5 +1,7 @@
-﻿public static class Priority {
-    public static void Test() {
+﻿public static class Priority
+{
+    public static void Test()
+    {
         // TODO Problem 2 - Write and run test cases and fix the code to match requirements
         // Example of creating and using the priority queue
         var priorityQueue = new PriorityQueue();
@@ -8,22 +10,58 @@
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: What happens if there are more than one items that are listed as highest priority, 
+        //           the the item that is closest to the front of the queue should be removed and its value returned.
+        // Expected Result: Dequeue should return the first priority of the highest number
         Console.WriteLine("Test 1");
+        priorityQueue.Enqueue("Take test", 4);
+        Console.WriteLine(priorityQueue);
+        var lastValueTest3 = priorityQueue.Dequeue();
+        Console.WriteLine($"Dequeued value: {lastValueTest3}");
+        Console.WriteLine(priorityQueue);
 
-        // Defect(s) Found: 
+        // Defect(s) Found: Dequeue is removing wrong instance of high priority
 
         Console.WriteLine("---------");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
-        Console.WriteLine("Test 2");
+        // Scenario: The Enqueue function shall add an item to the back of the queue.
 
-        // Defect(s) Found: 
+        // Expected Result: Add 3 elements to the queue
+        Console.WriteLine("Test 2");
+        priorityQueue.Enqueue("School", 4);
+        priorityQueue.Enqueue("Family", 1);
+        priorityQueue.Enqueue("Church", 2);
+        Console.WriteLine(priorityQueue);
+        // Defect(s) Found: None
 
         Console.WriteLine("---------");
+
+        // Test 3
+        // Scenario: Is there an error message displayed if queu is empty.
+        // Expected Result: Error message should display
+        Console.WriteLine("Test 3");
+        var priorityQueue2 = new PriorityQueue();
+        // Console.WriteLine(priorityQueue);
+        priorityQueue2.Dequeue();
+
+        // Defect(s) Found: none
+
+        Console.WriteLine("---------");
+
+        // Test 4
+        // Scenario: Dequeue should remove the item with the highest priority and return its value.
+        // Expected Result: Dequeue should return the highest number
+        Console.WriteLine("Test 2");
+        priorityQueue.Enqueue("Take kids to school", 6);
+        priorityQueue.Enqueue("Make dinner", 6);
+        priorityQueue.Enqueue("Do homework", 4);
+
+        var lastValueTest2 = priorityQueue.Dequeue();
+        Console.WriteLine($"Dequeued value: {lastValueTest2}");
+        Console.WriteLine(priorityQueue);
+        // Defect(s) Found: Dequeue wasn't removing the element from the list
+
 
         // Add more Test Cases As Needed Below
     }
--- week02/code/TakingTurns.cs
+++ week02/code/TakingTurns.cs
@@ -1,5 +1,7 @@
-﻿public static class TakingTurns {
-    public static void Test() {
+﻿public static class TakingTurns
+{
+    public static void Test()
+    {
         // TODO Problem 1 - Run test cases and fix the code to match requirements
         // Test Cases
 
@@ -15,7 +17,9 @@
         // Console.WriteLine(players);    // This can be un-commented out for debug help
         while (players.Length > 0)
             players.GetNextPerson();
-        // Defect(s) Found: 
+        // Defect(s) Found: The Enqueue function in PersonQueue has the value insert at the front of the queue. 
+        // Fixes: I changed the insert to an add function
+
 
         Console.WriteLine("---------");
 
@@ -28,7 +32,8 @@
         players.AddPerson("Bob", 2);
         players.AddPerson("Tim", 5);
         players.AddPerson("Sue", 3);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++)
+        {
             players.GetNextPerson();
             // Console.WriteLine(players);
         }
@@ -38,7 +43,7 @@
         while (players.Length > 0)
             players.GetNextPerson();
 
-        // Defect(s) Found: 
+        // Defect(s) Found: None 
 
         Console.WriteLine("---------");
 
@@ -52,15 +57,17 @@
         players.AddPerson("Tim", 0);
         players.AddPerson("Sue", 3);
         // Console.WriteLine(players);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
+        {
             players.GetNextPerson();
             // Console.WriteLine(players);
         }
-        // Defect(s) Found: 
+        // Defect(s) Found: Tim wouldn't repeat.
+        // Fixes: I added an else if statement to check for infinite turns.
 
         Console.WriteLine("---------");
 
-         // Test 4
+        // Test 4
         // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
         // Run 10 times.
         // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
@@ -69,11 +76,12 @@
         players.AddPerson("Tim", -3);
         players.AddPerson("Sue", 3);
         // Console.WriteLine(players);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
+        {
             players.GetNextPerson();
             // Console.WriteLine(players);
         }
-        // Defect(s) Found: 
+        // Defect(s) Found: Tim was being enqued again.
 
         Console.WriteLine("---------");
 
@@ -83,6 +91,6 @@
         Console.WriteLine("Test 5");
         players = new TakingTurnsQueue();
         players.GetNextPerson();
-        // Defect(s) Found:
+        // Defect(s) Found: None
     }
 }

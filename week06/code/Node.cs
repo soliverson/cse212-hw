4bd4a19856a3080b1b764f4d56cff5e676f145a1
soliverson <stacy.oliverson@gmail.com>
2024-04-04 19:43:45 -0600

    Finished Week 6

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,21 +1,26 @@
-public class Node {
+public class Node
+{
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data) {
+    public Node(int data)
+    {
         this.Data = data;
     }
 
-    public void Insert(int value) {
-        if (value < Data) {
+    public void Insert(int value)
+    {
+        if (value < Data)
+        {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else {
+        else if (value > Data)
+        {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
@@ -24,13 +29,30 @@ public class Node {
         }
     }
 
-    public bool Contains(int value) {
+    public bool Contains(int value)
+    {
         // TODO Start Problem 2
+        if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data && Left != null)
+        {
+            return Left.Contains(value);
+        }
+        else if (value > Data && Right != null)
+        {
+            return Right.Contains(value);
+        }
         return false;
     }
 
-    public int GetHeight() {
+    public int GetHeight()
+    {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
+        //return 0; // Replace this line with the correct return statement(s)
     }
 }
